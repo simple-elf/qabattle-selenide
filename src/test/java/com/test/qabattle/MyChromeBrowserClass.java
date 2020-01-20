600b3df4bfdb7603ab9b2b17e899bcb40b86c919
Aaftakhov <simple1elf@gmail.com>
2020-01-20 22:18:47 +0500

    driver providers

---
+++ src/test/java/com/test/qabattle/MyChromeBrowserClass.java
@@ -0,0 +1,62 @@
+package com.test.qabattle;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.model.Status;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.qameta.allure.Allure.step;
+
+public class MyChromeBrowserClass implements WebDriverProvider {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public WebDriver createDriver(DesiredCapabilities capabilities) {
+        WebDriverManager.chromedriver().setup();
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, getChromeOptions());
+
+        try {
+            return new ChromeDriver(capabilities);
+        } catch (Exception e) {
+            e.printStackTrace();
+            step("ChromeDriver error: " + e.getMessage(), Status.FAILED);
+            throw e;
+            //return null;
+        }
+    }
+
+    public static ChromeOptions getChromeOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("disable-infobars");
+        chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        chromeOptions.setExperimentalOption("useAutomationExtension", false);
+
+        //chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        //chromeOptions.addArguments("disable-popup-blocking", "true");
+
+        //chromeOptions.addArguments("--enable-logging=stderr --v=1");
+        //if (BaseTestClass.isUnix())
+        //    chromeOptions.setBinary("/usr/bin/google-chrome");
+
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        chromeOptions.setExperimentalOption("prefs", prefs);
+
+        return chromeOptions;
+    }
+
+}
---
+++ src/test/java/com/test/qabattle/MyRemoteWebDriverClass.java
@@ -0,0 +1,53 @@
+package com.test.qabattle;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.qameta.allure.model.Status;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static io.qameta.allure.Allure.step;
+
+public class MyRemoteWebDriverClass implements WebDriverProvider {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public WebDriver createDriver(DesiredCapabilities capabilities) {
+        capabilities.setBrowserName("chrome");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, MyChromeBrowserClass.getChromeOptions());
+        capabilities.setCapability("screenResolution", "1920x1080x24");
+        //capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("timeZone", "Europe/Moscow");
+
+        //System.out.println("video.enabled: " + System.getProperty("video.enabled"));
+        if ("true".equals(System.getProperty("video.enabled"))) {
+            capabilities.setCapability("enableVideo", true);
+            capabilities.setCapability("videoFrameRate", 24);
+        }
+
+        System.out.println(capabilities);
+        try {
+            return new RemoteWebDriver(getGridHubUrl(), capabilities);
+        } catch (Exception e) {
+            e.printStackTrace();
+            step("RemoteDriver error: " + e.getMessage(), Status.FAILED);
+            throw e;
+            //return null;
+        }
+    }
+
+    public static URL getGridHubUrl() {
+        URL hostURL = null;
+        try {
+            hostURL = new URL(System.getProperty("selenoid.url", "http://127.0.0.1:4444/wd/hub"));
+        } catch (MalformedURLException e) {
+            //e.printStackTrace();
+        }
+        return hostURL;
+    }
+
+}
