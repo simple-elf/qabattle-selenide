9e823b5bc7b2a9db7088b6ef52e8e512c2e8a89c
Aaftakhov <simple1elf@gmail.com>
2020-01-20 23:11:47 +0500

    fix test to wait

--- src/test/java/com/test/qabattle/FirstTest.java
+++ src/test/java/com/test/qabattle/FirstTest.java
@@ -1,7 +1,10 @@
 package com.test.qabattle;
 
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.test.qabattle.AllureHelpers.takeScreenshot;
 
@@ -11,8 +14,11 @@ public class FirstTest extends BaseTestClass {
     public void test() {
         open("http://127.0.0.1:8080/");
         //open("http://212.237.55.99:8081/");
+
+        $("#registrationContainer").should(appear);
+        $(".card-header").shouldBe(visible).shouldHave(exactText("Welcome to Propeller Championship!"));
+
         takeScreenshot();
-        System.out.println("tests!!!");
     }
 
 }
